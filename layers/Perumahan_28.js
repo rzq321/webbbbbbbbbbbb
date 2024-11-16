d7c786cf52797be89fa9ef93e09d4d8b2049b64f
Muhammad Rizqi <muhammadrzqr4321@.com>
2024-11-16 10:45:09 +0700

    first init github

---
+++ layers/Perumahan_28.js
@@ -0,0 +1,1 @@
+var json_Perumahan_28 = {"type":"FeatureCollection","name":"Perumahan_28","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":null,"nama":"Perumahan Graha Timur"},"geometry":{"type":"Point","coordinates":[109.257279460022048,-7.425539149990425]}}]}
